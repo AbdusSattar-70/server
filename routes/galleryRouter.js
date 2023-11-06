a991b16ab343553f56e438f74f3e70bdba13c522
Abdus Sattar <sasorganicagro@gmail.com>
2023-11-06 20:09:06 +0600

    Add single image link

--- routes/galleryRouter.js
+++ routes/galleryRouter.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.post('/create', createGallery);
 router.get('/getOne', getGallery);
-router.get('/getAll', getGalleries);
+router.get('/get/:id', getGalleries);
 
 module.exports = router;
